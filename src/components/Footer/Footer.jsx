3ce34726fb83fd1e9c08a7d6b119aece3aae1886
Emmanuel <emmanuel.effiong361@gmail.com>
2022-01-03 13:14:29 +0100

    responsive footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+import styles from "./Footer.module.css";
+import { ReactComponent as Facebook } from "../../assets/shared/desktop/icon-facebook.svg";
+import { ReactComponent as Instagram } from "../../assets/shared/desktop/icon-instagram.svg";
+import { ReactComponent as Twitter } from "../../assets/shared/desktop/icon-twitter.svg";
+import { ReactComponent as Logo } from "../../assets/shared/desktop/logo.svg";
+
+const Footer = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.hr}>
+        <hr />
+      </div>
+      <div className={styles.logo}>
+        <a href="#.com">
+          <Logo />
+        </a>
+      </div>
+      <div className={styles.nav_links}>
+        <ul>
+          <li>
+            <a href="#.com">Home</a>
+          </li>
+          <li>
+            <a href="#.com">Headphones</a>
+          </li>
+          <li>
+            <a href="#.com">Speakers</a>
+          </li>
+          <li>
+            <a href="#.com">Earphones</a>
+          </li>
+        </ul>
+      </div>
+      <div className={styles.text}>
+        <p>
+          Audiophile is an all in one stop to fulfill your audio needs. We're a
+          small team of music lovers and sound specialists who are devoted to
+          helping you get the most out of personal audio. Come and visit our
+          demo facility - we’re open 7 days a week.
+        </p>
+      </div>
+      <div className={styles.icons}>
+        <Facebook className={styles.icon} />
+        <Twitter className={styles.icon} />
+        <Instagram className={styles.icon} />
+      </div>
+      <div className={styles.copyright}>
+        <p>Copyright 2021. All Rights Reserved</p>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import BestGear from "../../components/BestGear/BestGear";
+import Footer from "../../components/Footer/Footer";
 import Header from "../../components/Header/Header";
 import Speaker1 from "../../components/SPEAKER1/Speaker1";
 import classes from "./Home.module.css";
@@ -11,6 +12,7 @@ const Home = () => {
         <Speaker1 />
         <BestGear />
       </section>
+      <Footer />
     </main>
   );
 };

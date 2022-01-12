dfae5e137f75983829ca719ef9cb07c355e3a6eb
Emmanuel <emmanuel.effiong361@gmail.com>
2022-01-12 12:01:40 +0100

    rolling in functionalities

--- src/pages/ProductDetail/Productdetail.jsx
+++ src/pages/ProductDetail/Productdetail.jsx
@@ -1,39 +1,55 @@
-import React from 'react'
-import styles from './Productdetail.module.css'
-import Button from '../../components/Button/Button';
-const Productdetail = () => {
-    return (
-      <main>
-        <div>
-          <p>Go back</p>
-        </div>
-        <div className={styles.item_container}>
-          <div className={styles.item_container_image}>
-            <div className={styles.item_container_img} />
-          </div>
-          <div className={styles.item_container_text}>
-            <p>New product</p>
-            <h2>
-              yx1 wireless
-              <br />
-              earphones
-            </h2>
-            <h4>
-              Tailor your listening experience with bespoke dynamic drivers from
-              the new YX1 Wireless Earphones. Enjoy incredible high-fidelity
-              sound even in noisy environments with its active noise
-              cancellation feature.
-            </h4>
-            <div>
-<div>
-    <input type="number" value="1"/>
-</div>
-            <Button label="see product" />
-            </div>
-          </div>
-        </div>
-      </main>
-    );
-}
+import React, { useState } from "react";
+import styles from "./Productdetail.module.css";
+import Button from "../../components/Button/Button";
+import img1 from "../../assets/product-yx1-earphones/desktop/image-category-page-preview.jpg";
 
-export default Productdetail
+const Productdetail = () => {
+  const [qty, setqty] = useState(1);
+  const reduceQty = () => {
+    if (qty === 1) return;
+    setqty(qty-1);
+  };
+  const increaseQty = () => {
+    setqty(qty+1);
+  };
+
+  return (
+    <main>
+      <div>
+        <p>Go back</p>
+      </div>
+      <div className={styles.item_container}>
+        <div className={styles.item_container_image}>
+          <img src={img1} alt="" className={styles.item_containerImg} />
+        </div>
+        <div className={styles.item_container_text}>
+          <p>New product</p>
+          <h2>
+            yx1 wireless
+            <br />
+            earphones
+          </h2>
+          <h4>
+            Tailor your listening experience with bespoke dynamic drivers from
+            the new YX1 Wireless Earphones. Enjoy incredible high-fidelity sound
+            even in noisy environments with its active noise cancellation
+            feature.
+          </h4>
+          <div className={styles.product_amount}>
+            $ <span>400</span>
+          </div>
+          <div className={styles.product_atk}>
+            <div className={styles.productdetail_input}>
+              <button onClick={reduceQty}>-</button>
+              <span className={styles.productdetail_qty}>{qty}</span>
+              <button onClick={increaseQty}>+</button>
+            </div>
+            <Button label="add to cart" />
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default Productdetail;
